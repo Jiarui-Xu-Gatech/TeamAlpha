d5e907e5c6ce9c0fed458ed73d19506a2e7c5396
陳 義龍 <yi-chen@apptec.co.jp>
2022-07-02 00:57:43 -0400

    add navMesh, trigger

--- Assets/Scripts/npcController.cs
+++ Assets/Scripts/npcController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 [RequireComponent(typeof(Animator))]
 public class npcController : MonoBehaviour
@@ -8,12 +9,16 @@ public class npcController : MonoBehaviour
     private Animator anim;
     public Rigidbody player;
     public float speed = 0.02f;
+    private NavMeshAgent navMeshAgent;
     Vector3 playerPos = Vector3.zero;
+    float remainDis;
     // Start is called before the first frame update
 
     void Awake()
     {
         anim = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        anim.SetBool("isWalk", false);
     }
 
     void OnTriggerEnter(Collider c)
@@ -46,24 +51,24 @@ public class npcController : MonoBehaviour
     void Update()
     {
         playerPos = player.transform.position;
+        remainDis = (playerPos - navMeshAgent.transform.position).magnitude;
 
         if (anim.GetBool("isWalk") == true)
         {
-            this.transform.position += Vector3.Normalize(player.position - this.transform.position) * speed;
-            this.transform.LookAt(playerPos, Vector3.up);
+            //this.transform.position += Vector3.Normalize(player.position - this.transform.position) * speed;
+            //this.transform.LookAt(playerPos, Vector3.up);
+            navMeshAgent.SetDestination(player.position);
+            anim.SetBool("isJump", false);
+            anim.SetBool("isWalk", true);
+            anim.SetBool("isIdle", false);
         }
-
-        if((player.position - this.transform.position).magnitude < 0.5)
+        
+        if(remainDis < 5)
         {
             anim.SetBool("isJump", true);
             anim.SetBool("isWalk", false);
             anim.SetBool("isIdle", false);
-        }
-        else
-        {
-            anim.SetBool("isJump", false);
-            anim.SetBool("isWalk", true);
-            anim.SetBool("isIdle", false);
+            navMeshAgent.isStopped = true;
         }
     }
 }
